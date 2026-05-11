3d1640806305c2358ec00833af05878f2f9efda1
Santiago <229399078+saintbelts@users.noreply.github.com>
2026-05-10 18:17:36 -0700

    Update Fibonacci_Sequence.java

--- c/mid-complexity/c-java-menu-system/java-programs/Fibonacci_Sequence.java
+++ c/mid-complexity/c-java-menu-system/java-programs/Fibonacci_Sequence.java
@@ -1,12 +1,3 @@
-/*
-2. Escribir un programa para encontrar la secuencia de Fibonacci. La secuencia es al 
-siguiente: 
- 
-F= 0,1,1,2,3,5,8,13,21,34,55, ......................................., n 
- 
-El primer elemento es 0, el segundo es 1 y cada elemento restante es la suma de los 
-dos anteriores. El programa debe solicitar el número límite de la serie.
-*/
 import java.util.Scanner;
 public class Fibonacci_Sequence {
     public static void main (String[] args) {
